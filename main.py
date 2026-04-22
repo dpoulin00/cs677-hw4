a4d3a4b0ff5314fd22ec6e28b90007a0fd760dc8
Tristan Carel <tcarel@umass.edu>
2026-04-22 10:52:54 -0400

    testing

--- main.py
+++ main.py
@@ -61,10 +61,13 @@ def run_network(network: dict[int, p2p.P2PNode], run_time:int, stop_network:bool
     #log_path = Path("leader_log.csv")
     log_path = Path("leader_log.csv")
     clock_path = Path("leader_clock")
+    timestamp_path = Path("node_0_request_timestamps.csv")
     if log_path.exists():
         log_path.unlink()
     if clock_path.exists():
         clock_path.unlink()
+    if timestamp_path.exists():
+        timestamp_path.unlink()
     time.sleep(1)  # time for logs to be deleted
     process_list = []
     for nid in network.keys():
--- performance_testing.py
+++ performance_testing.py
@@ -16,7 +16,7 @@ from main import make_random_network
 import matplotlib.pyplot as plt
 
 
-def make_performance_test_network(num_nodes: int, start_port:int, max_requests:int, nodes_to_limit:list) -> dict[int, p2p.P2PNode]:
+def make_performance_test_network(num_nodes: int, start_port, max_requests=-1, node_to_set=None) -> dict[int, p2p.P2PNode]:
     """
     Creates a random networ of nodes. Returns dict, whose keys are ports
     and whose values are the nodse.
@@ -30,9 +30,8 @@ def make_performance_test_network(num_nodes: int, start_port:int, max_requests:i
         node_view_of_network = node_ports.copy()
         del node_view_of_network[id]
         # Three possible roles, BUYER, SELLER, and BUYER_AND_SELLER
-        is_buyer = (True if role in [p2p.Role.BUYER.name, p2p.Role.BUYER_AND_SELLER.name] or id in nodes_to_limit else False)
-        is_seller = (True if role in [p2p.Role.SELLER.name, p2p.Role.BUYER_AND_SELLER.name] and id not in nodes_to_limit else False)
-        max_requests = max_requests if id in nodes_to_limit else -1
+        is_buyer = (True if role in [p2p.Role.BUYER.name, p2p.Role.BUYER_AND_SELLER.name] or node_to_set is not None and id==node_to_set else False)
+        is_seller = (True if role in [p2p.Role.SELLER.name, p2p.Role.BUYER_AND_SELLER.name] and (node_to_set is None or id !=node_to_set) else False)
         network[curr_port_number] = p2p.P2PNode(id=id,
                                                 port_number=curr_port_number,
                                                 is_buyer=is_buyer,
@@ -44,20 +43,10 @@ def make_performance_test_network(num_nodes: int, start_port:int, max_requests:i
 
 if __name__ == "__main__":
     start_port = 49152
-    log_path = Path("leader_log.csv")
-    clock_path = Path("leader_clock")
-    timestamp_path = Path("node_0_request_timestamps_new.csv")
-    if log_path.exists():
-        log_path.unlink()
-    if clock_path.exists():
-        clock_path.unlink()
-    if timestamp_path.exists():
-        timestamp_path.unlink()
-    time.sleep(1)  # time for logs to be deleted
-    network = make_performance_test_network(num_nodes=6, start_port=start_port, max_requests=1000, nodes_to_limit=[0])
-    test = 2
+    network = make_performance_test_network(num_nodes=6, start_port=start_port, max_requests=1000, node_to_set=0)
     run_network(network=network, run_time=10000, stop_network=True)
 
+    timestamp_path = Path("node_0_request_timestamps.csv")
     request_timestamps = pd.read_csv(timestamp_path)
 
     x_data = []
@@ -65,11 +54,8 @@ if __name__ == "__main__":
     x_data2 = []
 
     for i, row in request_timestamps.iterrows():
-        if row["received_time"] is not None:
-            x_data.append(row.received_time - row.init_time)
+        if row["acked_time"] is not None:
             x_data2.append(row.acked_time - row.init_time)
             y_data.append(random.random() * 20)
-    plt.scatter(x_data, y_data)
-    plt.show()
     plt.scatter(x_data2, y_data)
     plt.show()
